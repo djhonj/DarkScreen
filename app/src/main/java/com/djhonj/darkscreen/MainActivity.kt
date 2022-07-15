840cf099925b1732a1c542f2bc81671681949e02
Jhon Diaz <diazj691@gmail.com>
2022-07-14 21:25:01 -0500

    trabajando en home activity

--- app/src/main/java/com/djhonj/darkscreen/MainActivity.kt
+++ app/src/main/java/com/djhonj/darkscreen/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.djhonj.darkscreen
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getWindow().setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
     }
 }
