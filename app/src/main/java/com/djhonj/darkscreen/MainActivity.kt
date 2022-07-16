bfb9105d2e7fb0242841741934f7aa4741098437
Jhon Diaz <diazj691@gmail.com>
2022-07-15 20:39:28 -0500

    touch action in setting icon

--- app/src/main/java/com/djhonj/darkscreen/MainActivity.kt
+++ app/src/main/java/com/djhonj/darkscreen/MainActivity.kt
@@ -4,12 +4,33 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getWindow().setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+        var btnSetting: ImageView = findViewById(R.id.btnSetting)
+
+        btnSetting.setOnClickListener {
+            Toast.makeText(this, "image", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSetting.setOnTouchListener { v, event ->
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    Toast.makeText(this@MainActivity, "DOWN", Toast.LENGTH_SHORT).show()
+                }
+                MotionEvent.ACTION_UP -> {
+                    Toast.makeText(this@MainActivity, "UP", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            true
+        }
     }
 }
